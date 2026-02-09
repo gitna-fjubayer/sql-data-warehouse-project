4fef9027dec199d020699aa17d18382c6b727da8
Ahnaf Jubayer Islam <aji889@uowmail.edu.au>
2026-02-09 12:14:17 +0600

    Create tables for CRM and ERP data in bronze layer
    Added multiple tables for customer, product, sales, and ERP data in the bronze layer.

--- scripts/bronze_layer.sql
+++ scripts/bronze_layer.sql
@@ -1,5 +1,9 @@
 -- Create tables for the bronze layer
--- cust_info table
+-- crm tables
+-- customer information table
+
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL 
+    DROP TABLE bronze.crm_cust_info;
 
 CREATE TABLE bronze.crm_cust_info (
   cst_id NVARCHAR (50),
@@ -10,6 +14,9 @@ CREATE TABLE bronze.crm_cust_info (
   cst_gndr NVARCHAR (50),
   cst_create_date DATE 
 );
+-- product info table
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
+     DROP TABLE bronze.crm_prd_info;
 
 CREATE TABLE bronze.crm_prd_info (
   prd_id INT,
@@ -20,6 +27,10 @@ CREATE TABLE bronze.crm_prd_info (
   prd_start_dt DATETIME,
   prd_end_dt DATETIME
 );
+-- sales detail table
+
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
+     DROP TABLE bronze.crm_sales_details;
 
 CREATE TABLE bronze.crm_sales_details (
   sls_ord_num NVARCHAR (50),
@@ -33,3 +44,35 @@ CREATE TABLE bronze.crm_sales_details (
   sls_price INT
 );
 
+-- erp tables
+-- cust_az12 table
+
+IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
+     DROP TABLE bronze.erp_cust_az12;
+
+CREATE TABLE bronze.erp_cust_az12 (
+  cid NVARCHAR (50),
+  bdate DATE,
+  gen NVARCHAR (50)
+);
+
+-- loc_a101 table
+
+IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+
+CREATE TABLE bronze.erp_loc_a101 (
+  cid NVARCHAR (50),
+  cntry NVARCHAR (50)
+);
+-- PX_CAT_G1V2 table
+
+IF OBJECT_ID (' bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE  bronze.erp_px_cat_g1v2;
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+  id NVARCHAR (50),
+  cat NVARCHAR (50),
+  subcat NVARCHAR (50),
+  maintenance NVARCHAR (50)
+  );
