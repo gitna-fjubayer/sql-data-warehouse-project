d6aa5827e79c064bf3d7865b4ab0bc611eecd5c5
Ahnaf Jubayer Islam <aji889@uowmail.edu.au>
2026-02-15 02:16:50 +0600

    Add data insertion logic to crm_sales_details
    Insert cleaned data into crm_sales_details table with validations and recalculations for date and sales fields.

--- scripts/silver_layer/clean_load.sql
+++ scripts/silver_layer/clean_load.sql
@@ -65,3 +65,46 @@ FROM bronze.crm_prd_info;
 Verify cleaned table with 
 SELECT * FROM silver.crm_prd_info;
 */
+
+-- Insert cleaned data into crm_sales_details table
+
+INSERT INTO silver.crm_sales_details (
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+sls_order_dt,
+sls_ship_dt,
+sls_due_dt,
+sls_sales,
+sls_quantity,
+sls_price)
+
+SELECT 
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+CASE WHEN sls_order_dt = 0 OR len(sls_order_dt) != 8 THEN NULL
+  ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+END AS sls_order_dt,
+CASE WHEN sls_ship_dt = 0 OR len(sls_ship_dt) != 8 THEN NULL
+  ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+END AS sls_ship_dt,
+CASE WHEN sls_due_dt = 0 OR len(sls_due_dt) != 8 THEN NULL
+  ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+END AS sls_due_dt,                                                               -- Handling invalid data and casting the date from integer DATE format
+CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * abs(sls_price)
+  THEN sls_quantity * ABS(sls_price)
+  ELSE sls_sales
+END AS sls_sales,    -- Recalculating the sales if original values is missing or incorrect
+sls_quantity,
+CASE WHEN sls_price IS NULL OR sls_price <= 0 
+  THEN sls_sales / NULLIF (sls_quantity, 0)
+  ELSE sls_price
+END AS sls_price          -- Derive price if original value is invalid
+FROM bronze.crm_sales_details;
+
+/*
+Verify cleaned table with 
+SELECT * FROM silver.crm_sales_details;
+*/
+
