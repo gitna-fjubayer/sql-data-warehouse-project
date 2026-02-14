907b1d214ae3695156f978aea64a33c2e284cf34
Ahnaf Jubayer Islam <aji889@uowmail.edu.au>
2026-02-14 21:12:37 +0600

    Add SQL for inserting cleaned product data into silver layer
    Insert cleaned data into the silver layer product info table with transformations and mappings.

--- scripts/silver_layer/clean_load.sql
+++ scripts/silver_layer/clean_load.sql
@@ -1,5 +1,6 @@
 -- Silver layer data cleaning
 -- Insert cleaned data into silver layer cust_info table
+
 INSERT INTO silver.crm_cust_info (
   cst_id,
   cst_key,
@@ -32,4 +33,35 @@ WHERE cst_id IS NOT NULL
 /*
 Verify cleaned table with 
 SELECT * FROM silver.crm_cust_info;
+*/
+
+-- Insert cleaned data into silver layer prd_info table
+
+INSERT INTO silver.crm_prd_info (
+prd_id,
+cat_id,
+prd_key,
+prd_nm,
+prd_cost,
+prd_line,
+prd_start_dt,
+prd_end_dt)
+SELECT 
+prd_id,
+REPLACE(SUBSTRING (prd_key, 1, 5), '-', '_') AS cat_id,   -- Extract category id
+SUBSTRING (prd_key, 7, LEN(prd_key)) AS prd_key,          -- Extract product key
+prd_nm,
+ISNULL(prd_cost, 0) AS prd_cost,
+CASE WHEN UPPER (TRIM(prd_line)) = 'M' THEN 'Mountain'
+     WHEN UPPER (TRIM(prd_line)) = 'S' THEN 'Other Sales'
+     WHEN UPPER (TRIM(prd_line)) = 'R' THEN 'Road'
+     WHEN UPPER (TRIM(prd_line)) = 'T' THEN 'Touring'
+     ELSE 'N/A'
+END AS prd_line,                    -- Mapping product line codes to their descriptive values
+CAST (prd_start_dt AS DATE) AS prd_start_dt,
+CAST (LEAD (prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) -1 AS DATE) AS prd_end_dt   -- Calculate end date as one day before the next start date 
+FROM bronze.crm_prd_info;
+/*
+Verify cleaned table with 
+SELECT * FROM silver.crm_prd_info;
 */
