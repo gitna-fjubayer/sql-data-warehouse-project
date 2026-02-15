a35810046172b4ecbda30b38822801bc6b3907de
Ahnaf Jubayer Islam <aji889@uowmail.edu.au>
2026-02-15 21:21:17 +0600

    Document Load Bronze Layer stored procedure
    Add documentation for the Load Bronze Layer stored procedure, detailing its purpose and actions.

--- scripts/bronze_layer/load_data.sql
+++ scripts/bronze_layer/load_data.sql
@@ -1,3 +1,19 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+===============================================================================
+*/
+
 /* To verify quality of loaded data -
 SELECT * FROM bronze.crm/erp_tablename
 To verify if right amount of rows are loaded -
