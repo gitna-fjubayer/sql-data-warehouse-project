f50390c87136246a74f553f6e1ea700d6b5c0f98
Ahnaf Jubayer Islam <aji889@uowmail.edu.au>
2026-02-15 21:19:54 +0600

    Add header comment for create_table.sql
    Added a header comment explaining the purpose of the script.

--- scripts/bronze_layer/create_table.sql
+++ scripts/bronze_layer/create_table.sql
@@ -1,3 +1,15 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
+
 -- Create tables for the bronze layer
 -- crm tables
 -- customer information table
