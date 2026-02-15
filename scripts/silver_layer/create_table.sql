b022886f5cc5754315dd09d80523289951017cad
Ahnaf Jubayer Islam <aji889@uowmail.edu.au>
2026-02-15 21:18:56 +0600

    Create DDL script for silver layer tables
    Add DDL script to create tables in the silver schema.

--- scripts/silver_layer/create_table.sql
+++ scripts/silver_layer/create_table.sql
@@ -1,3 +1,14 @@
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
 -- Create tables for the silver layer
 -- crm tables
 -- customer information table
